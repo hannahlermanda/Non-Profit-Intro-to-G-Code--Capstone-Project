392ebf845c8bb019daf66fb59597d1a0d048ca78
hannahlermanda <hannahlermanda@gmail.com>
2022-12-02 18:15:25 -0800

    nav bar

--- javascript.js
+++ javascript.js
@@ -47,7 +47,7 @@ function showSlides(n) {
 }
 
 //dark-light mode
-function myFunction() {
+function myFunctionLight() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
@@ -65,3 +65,19 @@ function myFunction() {
   }
   document.getElementById("demo").innerHTML = text;
 }
+
+//navbar
+const navbarToggle = navbar.querySelector("#navbar-toggle");
+const navbarMenu = document.querySelector("#navbar-menu");
+const navbarLinksContainer = navbarMenu.querySelector(".navbar-links");
+let isNavbarExpanded = navbarToggle.getAttribute("aria-expanded") === "true";
+
+const toggleNavbarVisibility = () => {
+  isNavbarExpanded = !isNavbarExpanded;
+  navbarToggle.setAttribute("aria-expanded", isNavbarExpanded);
+};
+
+navbarToggle.addEventListener("click", toggleNavbarVisibility);
+
+navbarLinksContainer.addEventListener("click", (e) => e.stopPropagation());
+navbarMenu.addEventListener("mouseleave", toggleNavbarVisibility);
